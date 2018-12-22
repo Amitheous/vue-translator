31d5b363eb04f3303749d1fea4ba6c8ebb42edc3
Matthew Kartchner <mkartch12@gmail.com>
2018-12-22 17:49:41 +0800

    first commit/finished v1

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card bg-success text-center col-md-6 offset-3 mt-4 pt-0 px-0" id="app">
+    <div class="card-header">
+    <h1 class="card-title mt-3">Word Translator</h1>
+    <h5 class="card-subtitle mt-3 mb-2">Powered by Vue.js</h5>
+    <h5 class="card-subtitle mt-3 mb-2">Powered by <a target="_blank" class="text-light" href="http://translate.yandex.com/">Yandex.Translate</a></h5>
+    </div>
+    <div class="card-body">
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
+    </div>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name: 'App',
+  components:{
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return{
+      translatedText:''
+    }
+  },
+  methods: {
+    // text from the formSubmit value being emitted from translateform.vue
+    translateText:function(text, language) {
+      this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181219T043611Z.a720d5bbefbbde16.b3f4baa4a5bb46b4e1ecb7a36305c764d097c3a4&lang=${language}&text=${text}`)
+      .then((response) => {
+        this.translatedText = response.body.text[0];
+      });
+    }
+  }
+}
+</script>
+
+<style>
+a{
+  text-decoration-line: underline;
+}
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="translateForm">
+    <form class="" v-on:submit="formSubmit">
+      <div class="form-group row mt-2">
+      <input class="form-control col-md-5 mx-auto" type="text" v-model="textToTranslate" placeholder="Enter a word">
+      <select class="form-control col-md-4 mx-auto" v-model="language">
+        <option value="es">Spanish</option>
+        <option value="ko">Korean</option>
+        <option value="fr">French</option>
+        <option value="pt">Portuguese</option>
+        <option value="ru">Russian</option>
+        <option value="sv">Swedish</option>
+        <option value="en">English</option>
+      </select>
+      <input class="btn btn-dark text-light mx-auto" type="submit" value="Translate">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data(){
+    return {
+      textToTranslate:'',
+      language:''
+    }
+  },
+  // Runs only when the elements included in these methods are created
+  created(){
+    this.language = 'es';
+  },
+  methods: {
+    formSubmit(e){
+      // 'Emits' the textToTranslate value to the formSubmit attribute(in App.vue)
+      this.$emit('formSubmit', this.textToTranslate, this.language)
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
